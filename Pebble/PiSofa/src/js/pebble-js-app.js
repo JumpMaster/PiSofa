46309da31abaee47f42bcfc081771553d824b833
Kevin Cooper <kevin@cooper.uk.com>
2014-01-12 16:59:34 +0000

    Updated Pebble code and PiSofa V2 PCB

--- Pebble/PiSofa/src/js/pebble-js-app.js
+++ Pebble/PiSofa/src/js/pebble-js-app.js
@@ -1,30 +1,38 @@
-var baseUrl = "sofa.cooper.local";
+var baseUrl = "0.0.0.0";
 
 function sendRequest(url) {
 	//var response;
 	var req = new XMLHttpRequest();
 	// build the GET request
 	req.open('GET', url, true);
-	
-	req.onload = function(e) {
-		if (req.readyState == 4) {
+	req.onreadystatechange = function(e) {
+		// req.readyState
+		// 0: request not initialized 
+		// 1: server connection established
+		// 2: request received 
+		// 3: processing request 
+		// 4: request finished and response is ready
+		if (req.readyState == 0) {
+			Pebble.showSimpleNotificationOnPebble("ERROR 2", "Error code from HTTP request was " + req.status.toString());
+        	console.log("Request returned error code " + req.status.toString());
+		} else if (req.readyState == 4) {
 			if(req.status != 200) {
-				Pebble.showSimpleNotificationOnPebble("ERROR", "Error code from HTTP request was " + req.status.toString());
+				Pebble.showSimpleNotificationOnPebble("ERROR 1", "Error code from HTTP request was " + req.status.toString());
         		console.log("Request returned error code " + req.status.toString());
 			}
-		} else {
-			Pebble.showSimpleNotificationOnPebble("ERROR", "Error code from HTTP request was " + req.status.toString());
-        	console.log("Request returned error code " + req.status.toString());
-      	}
+		}
     }
-	
 	req.send(null);
 }
 
 
 Pebble.addEventListener("ready",
-  function(e) {
-    console.log("JavaScript app ready and running!");
+	function(e) {
+    	console.log("JavaScript app ready and running!");
+		var savedSettings = localStorage.getItem('piSofaConfig');
+		var configuration = JSON.parse(savedSettings);
+		console.log("Configuration API: " + configuration.api);
+		baseUrl = configuration.api;
   }
 );
 
@@ -48,5 +56,19 @@ Pebble.addEventListener("appmessage",
 	  }
 	  console.log(url);
 	  sendRequest(url);
+  }
+);
+
+Pebble.addEventListener('showConfiguration', function(e) {
+	Pebble.openURL('http://oncloudvirtual.com/pisofa.html')
+});
+
+Pebble.addEventListener("webviewclosed",
+  function(e) {
+	  console.log("Configuration window returned: " + e.response);
+	  var configuration = JSON.parse(e.response);
+	  console.log("Configuration window returned: " + configuration.api);
+	  baseUrl = configuration.api;
+	  localStorage.setItem('piSofaConfig', e.response);
   }
 );
